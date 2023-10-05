28524e7572a2e512b04c7b335ac983b46f8119c6
Zane Shannon <z@zcs.me>
2023-10-05 00:32:08 -0700

    WIP streaming

--- examples/whisper.swiftui/whisper.swiftui.demo/Models/WhisperState.swift
+++ examples/whisper.swiftui/whisper.swiftui.demo/Models/WhisperState.swift
@@ -5,36 +5,111 @@ import SwiftUI
 
 actor WhisperStream {
     private var cancellables: Set<AnyCancellable> = []
-    private var context: OpaquePointer?
-    
+    private var ctxBig: OpaquePointer?
+    private var ctxLittle: OpaquePointer?
+    private var paramsBig: whisper_full_params
+    private var paramsLittle: whisper_full_params
+    private var source: URL
+
     init(model: URL, source: URL) {
-        self.context = whisper_init_from_file(model.path())
+        ctxBig = whisper_init_from_file(model.path())
+        ctxLittle = whisper_init_from_file(model.path())
+        paramsBig = whisper_full_default_params(WHISPER_SAMPLING_GREEDY)
+        paramsBig.print_realtime = false
+        paramsBig.print_progress = false
+        paramsBig.print_timestamps = false
+        paramsBig.max_tokens = 0
+//        params.language = "".cString(using: .utf8)
+        paramsBig.print_special = false
+        paramsBig.translate = false
+        paramsBig.n_threads = 2
+        paramsBig.offset_ms = 0
+        paramsBig.no_context = true
+        paramsBig.single_segment = false
+
+        paramsLittle = whisper_full_default_params(WHISPER_SAMPLING_GREEDY)
+        paramsLittle.print_realtime = false
+        paramsLittle.print_progress = false
+        paramsLittle.print_timestamps = false
+        paramsLittle.max_tokens = 0
+//        paraLittlelanguage = "".cString(using: .utf8)
+        paramsLittle.print_special = false
+        paramsLittle.translate = false
+        paramsLittle.n_threads = 2
+        paramsLittle.offset_ms = 0
+        paramsLittle.no_context = true
+        paramsLittle.single_segment = false
+        self.source = source
+//        "en".withCString { en in
+//            params.language = en
+//        }
     }
-    
+
     deinit {
-        whisper_free(context)
+        whisper_free(ctxBig)
+        whisper_free(ctxLittle)
     }
-    
+
     func start() {
-        Timer.publish(every: 0.03, on: .main, in: .common)
-            .autoconnect()
+        let timer = Timer.publish(every: 0.03, on: .main, in: .common).autoconnect()
+        timer
             .scan(0) { totalSeconds, _ in
                 totalSeconds + 1
             }.map { (totalSeconds: Int) -> Double in
                 Double(totalSeconds) * 0.03
             }
             .map { (totalSeconds: Double) -> Double in
-                print("LITTLE: \(totalSeconds)")
+//                print("LITTLE: \(totalSeconds)")
+                let from = 0
+                let to = Int(totalSeconds * 2 * 16000)
+//                if let samples = try? decodeWaveFile(self.source, from: from, to: to), let context = self.ctxLittle {
+//                    self.append(context: context, params: self.paramsLittle, samples: samples)
+//                    let text = self.getTranscription(context: context)
+//                    print("little: \(text)")
+//                }
                 return totalSeconds
             }
             .map { (t: Double) -> Int in Int(t / 2.0) }
             .removeDuplicates()
             .map { (t: Int) -> Int in t * 2 }
             .sink { totalSeconds in
-                print("BIG: \(totalSeconds)")
+                let t0 = Date.now
+                let to = totalSeconds * 2 * 16000
+                let from = 0 // max(0, to - (2 * 2 * 16000))
+                guard from < to else { return }
+                print("BIG: [\(from)-\(to)]")
+                if let samples = try? decodeWaveFile(self.source, from: from, to: to), let context = self.ctxBig {
+                    self.append(context: context, params: self.paramsBig, samples: samples)
+                    let text = self.getTranscription(context: context)
+                    // reset context?
+//                    context.state = whisper_init_state(context)
+                    print("BIG: \(text)")
+                }
+                if let data = try? Data(contentsOf: self.source), to > data.count {
+                    timer.upstream.connect().cancel()
+                }
+                let t1 = Date.now
+                print("big t: \(t1.timeIntervalSince(t0))")
             }.store(in: &cancellables)
     }
-    
+
+    func append(context: OpaquePointer, params: whisper_full_params, samples: [Float]) {
+        samples.withUnsafeBufferPointer { samples in
+            if whisper_full(context, params, samples.baseAddress, Int32(samples.count)) != 0 {
+                print("Failed to run the model")
+            } else {
+//                    whisper_print_timings(context)
+            }
+        }
+    }
+
+    func getTranscription(context: OpaquePointer) -> String {
+        var transcription = ""
+        for i in 0 ..< whisper_full_n_segments(context) {
+            transcription += String(cString: whisper_full_get_segment_text(context, i))
+        }
+        return transcription
+    }
 }
 
 @MainActor
@@ -73,6 +148,10 @@ class WhisperState: NSObject, ObservableObject, AVAudioRecorderDelegate {
         }
         if let model = modelUrl, let source = sampleUrl {
             stream = WhisperStream(model: model, source: source)
+            Task {
+                guard let stream = self.stream else { return }
+                await stream.start()
+            }
         }
     }
 
@@ -105,7 +184,7 @@ class WhisperState: NSObject, ObservableObject, AVAudioRecorderDelegate {
         do {
             canTranscribe = false
             messageLog += "Reading wave samples...\n"
-            let data = try readAudioSamples(url, from: 0, to: 2 * 2 * 16000)
+            let data = try readAudioSamples(url)
             messageLog += "Transcribing data...\n"
             await whisperContext.fullTranscribe(samples: data)
             let text = await whisperContext.getTranscription()
@@ -206,4 +285,8 @@ class WhisperState: NSObject, ObservableObject, AVAudioRecorderDelegate {
     private func onDidFinishRecording() {
         isRecording = false
     }
+}
+
+private func cpuCount() -> Int {
+    ProcessInfo.processInfo.processorCount
 }
